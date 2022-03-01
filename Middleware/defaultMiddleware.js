362a6549fb9e8e59e82e435b36dbf8e1381a227a
hnazmul <nazmul.w3@gmail.com>
2022-03-01 14:29:08 +0600

    cors problem

--- Middleware/defaultMiddleware.js
+++ Middleware/defaultMiddleware.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 module.exports = (app) => {
-    const allMiddleware = [express.static("public"), express.json(), express.urlencoded({ extended: true }), cors(), morgan("dev")];
+    const allMiddleware = [  cors(),express.static("public"), express.json(), express.urlencoded({ extended: true }), morgan("dev")];
 
     if (app) {
         allMiddleware.forEach((middleware) => app.use(middleware));
